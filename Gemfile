f773264c94248698ee9627b31fd5f93322c54fa4
Andrei Kislichenko <android.2net@gmail.com>
2026-02-26 12:19:42 -0500

    create data v2

--- Gemfile
+++ Gemfile
@@ -2,4 +2,4 @@
 
 source 'https://rubygems.org'
 
-gem 'relaton-nist', github: 'relaton/relaton-nist', branch: 'main'
+gem 'relaton-nist', "~> 2.0.0.alpha.1" # github: 'relaton/relaton-nist', branch: 'main'
--- crawler.rb
+++ crawler.rb
@@ -1,8 +1,17 @@
 # frozen_string_literal: true
 
-require 'relaton_nist'
+require 'relaton/nist/data_fetcher'
 
 FileUtils.rm_rf('data')
 FileUtils.rm Dir.glob('index*')
 
-RelatonNist::DataFetcher.fetch # output:'dir'
+Relaton::Nist::DataFetcher.fetch # output:'dir'
+
+index = Relaton::Index.find_or_create :nist, file: "#{Relaton::Nist::INDEXFILE}.yaml"
+
+Dir["static/*.yaml"].each do |file|
+  bib = Relaton::Nist::Item.from_yaml(File.read(file, encoding: "UTF-8"))
+  index.add_or_update bib.docidentifier[0].content, file
+end
+
+index.save
